6c3a2b3fdc32d28d986455d8d9aec3aaf4190bc4
njayem <nadine.el.mufti@gmail.com>
2023-07-30 20:26:28 -0400

    Build Rank component and add particles-bg background

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
+import ParticlesBg from "particles-bg";
 import "./App.css";
 
 function App() {
 	return (
 		<div className="App">
+			<ParticlesBg color="#ffffff" type="cobweb" bg={true} num={300} />
 			<Navigation />
 			<Logo />
 			<Rank />
