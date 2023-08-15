53905331a3a31a93c52411b04323a3c51eda039b
njayem <nadine.el.mufti@gmail.com>
2023-08-15 01:17:39 -0400

    Add Previews

--- SmartBrain_Front-End/src/components/Signin/Signin.js
+++ SmartBrain_Front-End/src/components/Signin/Signin.js
@@ -51,8 +51,8 @@ class Signin extends Component {
 	render() {
 		const { onRouteChange } = this.props;
 		return (
-			<article className=" centered br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-				<main className="pa4 black-80">
+			<article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+				<main className="pa4 black-80 center">
 					<div className="measure">
 						<fieldset id="sign_up" className="ba b--transparent ph0 mh0">
 							<legend className="f2 fw6 ph0 mh0">Sign In</legend>
